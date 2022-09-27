501b23d40f93d9bc2ec68b1d0ba7936e44bd25b2
unknown <gabriepolanco23@gmail.com>
2022-09-26 20:46:59 -0400

    first commit

---
+++ src/permisos.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include_once "includes/header.php"; ?>
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card">
+            <div class="card-header card-header-primary">
+                Permiso no Autorizado
+            </div>
+            <div class="card-body text-center">
+                NO tienes permiso para acceder a este módulo
+                <br>
+                <a class="btn btn-danger" href="./">Atras</a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include_once "includes/footer.php"; ?>
